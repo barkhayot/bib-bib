9634a9200fc960173d60d241beb400c8c05f2fb7
Barkhayot Juraev <coderviewer@gmail.com>
2023-01-19 16:52:05 +0900

    update

--- letsgo/settings.py
+++ letsgo/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'letsgo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "timer",
-        'USER': "postgres",
-        'PASSWORD': "gtb3E&rewBc!BP2U",
-        'HOST': "papamarketcorp.kr",
-        'PORT': "8432",
-    }
-}
-
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "timer",
+#         'USER': "postgres",
+#         'PASSWORD': "gtb3E&rewBc!BP2U",
+#         'HOST': "papamarketcorp.kr",
+#         'PORT': "8432",
+#     }
 # }
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
+}
 
 
 # Password validation
